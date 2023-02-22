5fdebbb8b0774386196d7525e2bc6c888162ca2a
abhishek-topwal <topwalabhi1998@gmail.com>
2023-02-22 17:10:26 +0530

    started working on the sysmon report module

--- OpenMATB/OpenMATB/report.py
+++ OpenMATB/OpenMATB/report.py
@@ -4,12 +4,28 @@ track_list = []
 comm_list = []
 
 
+
+def getSysmonScore(sysmon_list):
+    total_failures = 0
+    total_hits = 0
+    response_time = 0
+
+    for i,event in sysmon_list:
+        if event[5] == 'FAILURE\n':
+            total_failures += 1
+            if(sysmon_list[i+1][5]=='HIT\n'):
+
+
+        if event[3] == 'Success':
+            total_hits += 1 
+    print(total_failures)
+
 if __name__ == '__main__':
     participant_info = []
 
 
     # get path of log file
-    log_file_path = 'Logs/scenario_settings1_20230219_1833.log'
+    log_file_path = 'Logs/complete_sysmon_20230222_1513.log'
 
     # #open a file
     # f = open(log_file_path, 'w')
@@ -25,5 +41,4 @@ if __name__ == '__main__':
             if line[2] == 'SYSMON':
                 sysmon_list.append(line)
 
-print(sysmon_list)         
-
+getSysmonScore(sysmon_list)

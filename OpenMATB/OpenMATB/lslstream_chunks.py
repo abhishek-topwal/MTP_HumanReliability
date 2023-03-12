a8b416aadb2a634e6ba7ca44b5715ebc39272423
abhishek-topwal <84610922+abhishek-topwal@users.noreply.github.com>
2023-03-12 22:54:49 +0530

    Some changes

--- OpenMATB/OpenMATB/lslstream_chunks.py
+++ OpenMATB/OpenMATB/lslstream_chunks.py
@@ -1,4 +1,4 @@
-from pylsl import StreamInlet, resolve_stream
+import pylsl
 
 
 f = open('data.txt', 'w')
@@ -10,24 +10,27 @@ def main():
     print("looking for an EEG stream...")
 
     # use name of the stream to resolve it
-    stream_eeg = resolve_stream('name', 'obci_eeg1')
-    stream_pulse = resolve_stream('name', 'obci_eeg2')
+    stream_eeg = pylsl.resolve_stream('name', 'obci_eeg1')
+    stream_pulse = pylsl.resolve_stream('name', 'obci_eeg2')
 
     # create a new inlet to read from the stream
-    inlet_eeg = StreamInlet(stream_eeg[0])
-    inlet_pulse = StreamInlet(stream_pulse[0])
+    inlet_eeg = pylsl.StreamInlet(stream_eeg[0])
+    inlet_pulse = pylsl.StreamInlet(stream_pulse[0], processing_flags=pylsl.proc_ALL)
 
     while True:
         # get a new sample (you can also omit the timestamp part if you're not
         # interested in it)
-        chunk_eeg, timestamps_eeg = inlet_eeg.pull_chunk()
-        chunk_pulse, timestamps_pulse = inlet_pulse.pull_chunk()
+        # offset = inlet_pulse.time_correction()
+        # print(offset)
+        chunk_eeg, timestamps_eeg = inlet_eeg.pull_sample()
+        chunk_pulse, timestamps_pulse = inlet_pulse.pull_sample()
         # if timestamps_eeg:
         #     f.write("EEG ||"+str(timestamps_eeg) + '||' + str(chunk_eeg)+'\n')
             # print(timestamps, chunk)
+        f.write("Pulse ||"+str(timestamps_pulse) + '||' + str(chunk_pulse)+'\n')
 
-        if timestamps_pulse:
-            f.write("Pulse ||"+str(timestamps_pulse) + '||' + str(chunk_pulse)+'\n')
+        # if timestamps_pulse:
+        #     f.write("Pulse ||"+str(timestamps_pulse) + '||' + str(chunk_pulse)+'\n')
 
 
 if __name__ == '__main__':
